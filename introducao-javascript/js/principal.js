735fa6573113259b5e83380e4fd39e5c9f8fc597
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2018-12-16 06:06:05 -0200

    Criando elemento na tabela

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,4 +1,5 @@
 // Alterar o título da página
+var tituloHeader = document.querySelector('.titulo');
 document.querySelector('.titulo').textContent = 'Aparecida Nutricionista';
 
 // primeiro-paciente
@@ -39,3 +40,72 @@ for (var i = 0; i < pacientes.length; i++) {
     }
     // Fim  Lógica para exibir o IMC abaixo
 }
+
+/* simple escuta de click no titulo
+function mostrarMsg(){
+    console.log('Fui clicado!');
+}
+
+tituloHeader.addEventListener("click", mostrarMsg);
+*/
+
+// ==================================================================================================
+// ==================================================================================================
+
+// Eventos de clique
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
+botaoAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+    console.log('Oi eu sou o botao e fui clicado');
+
+    var form = document.querySelector('#form-adiciona');
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
+
+    var pacienteTr = document.createElement('tr'); // TR
+    var nomeTd = document.createElement('td');
+    var pesoTd = document.createElement('td');
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+});
+
+
+
+
+
+
+
+
+/* Simple botão de copiar para outro campo automaticamente
+
+var inputFrase = document.querySelector('.frase');
+var botao = document.querySelector('.botao');
+var copia = document.querySelector('.copia');
+
+function botaoHandler(){
+    copia.textContent = inputFrase.value;
+    inputFrase.value = '';
+}
+
+botao.addEventListener('click',botaoHandler);
+
+*/
