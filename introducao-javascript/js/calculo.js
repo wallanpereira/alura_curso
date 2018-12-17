b67b580014f53306ef0a4a7f773ea5044461d580
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2018-12-17 20:42:28 -0200

    iniciando aula 5

---
+++ introducao-javascript/js/calculo.js
@@ -0,0 +1,43 @@
+// Alterar o título da página
+var tituloHeader = document.querySelector('.titulo');
+document.querySelector('.titulo').textContent = 'Aparecida Nutricionista';
+
+// primeiro-paciente
+// var primeiroPaciente = document.querySelector('#primeiro-paciente td:nth-child(2)');
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+
+    // Inicio  Lógica para exibir o IMC abaixo
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    if (peso <= 0 || peso >= 500) {
+        paciente.querySelector('.info-imc').textContent = "Peso invalido!";
+        pesoEhValido = false;
+        paciente.classList.add('paciente-invalido');
+        /* .classList, que como o nome indica nos mostras classes que aquele HTML tem. */
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        paciente.querySelector('.info-imc').textContent = "Altura invalida!";
+        alturaEhValido = false;
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if (pesoEhValido && alturaEhValido) {
+        var imc = peso / (altura * altura); //  100 / 2.00 * 2.00
+
+        // Jogar valor do imc na tabela
+        paciente.querySelector('.info-imc').textContent = imc.toFixed(2); // toFixed defini quantidade de casas decimais
+    }
+    // Fim  Lógica para exibir o IMC abaixo
+}
--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,58 +1,3 @@
-// Alterar o título da página
-var tituloHeader = document.querySelector('.titulo');
-document.querySelector('.titulo').textContent = 'Aparecida Nutricionista';
-
-// primeiro-paciente
-// var primeiroPaciente = document.querySelector('#primeiro-paciente td:nth-child(2)');
-
-var pacientes = document.querySelectorAll('.paciente');
-
-for (var i = 0; i < pacientes.length; i++) {
-    var paciente = pacientes[i];
-
-    var tdAltura = paciente.querySelector('.info-altura');
-    var altura = tdAltura.textContent;
-
-    var tdPeso = paciente.querySelector('.info-peso');
-    var peso = tdPeso.textContent;
-
-    // Inicio  Lógica para exibir o IMC abaixo
-    var pesoEhValido = true;
-    var alturaEhValido = true;
-
-    if (peso <= 0 || peso >= 500) {
-        paciente.querySelector('.info-imc').textContent = "Peso invalido!";
-        pesoEhValido = false;
-        paciente.classList.add('paciente-invalido');
-        /* .classList, que como o nome indica nos mostras classes que aquele HTML tem. */
-    }
-
-    if (altura <= 0 || altura >= 3.00) {
-        paciente.querySelector('.info-imc').textContent = "Altura invalida!";
-        alturaEhValido = false;
-        paciente.classList.add('paciente-invalido');
-    }
-
-    if (pesoEhValido && alturaEhValido) {
-        var imc = peso / (altura * altura); //  100 / 2.00 * 2.00
-
-        // Jogar valor do imc na tabela
-        paciente.querySelector('.info-imc').textContent = imc.toFixed(2); // toFixed defini quantidade de casas decimais
-    }
-    // Fim  Lógica para exibir o IMC abaixo
-}
-
-/* simple escuta de click no titulo
-function mostrarMsg(){
-    console.log('Fui clicado!');
-}
-
-tituloHeader.addEventListener("click", mostrarMsg);
-*/
-
-// ==================================================================================================
-// ==================================================================================================
-
 // Eventos de clique
 
 var botaoAdicionar = document.querySelector('#adicionar-paciente');
@@ -93,27 +38,27 @@ botaoAdicionar.addEventListener("click",function(event){
 
     var tabelaPaciente = document.querySelector('#tabela-pacientes');
 
+
+    console.log(pacienteTr);
+
+    var tabelaPaciente = document.querySelector('#tabela-pacientes');
+
     tabelaPaciente.appendChild(pacienteTr);
 });
 
 
 
-
-
-
-
-
 /* Simple botão de copiar para outro campo automaticamente
 
-var inputFrase = document.querySelector('.frase');
-var botao = document.querySelector('.botao');
-var copia = document.querySelector('.copia');
+    var inputFrase = document.querySelector('.frase');
+    var botao = document.querySelector('.botao');
+    var copia = document.querySelector('.copia');
 
-function botaoHandler(){
-    copia.textContent = inputFrase.value;
-    inputFrase.value = '';
-}
+    function botaoHandler(){
+        copia.textContent = inputFrase.value;
+        inputFrase.value = '';
+    }
 
-botao.addEventListener('click',botaoHandler);
+    botao.addEventListener('click',botaoHandler);
 
 */
