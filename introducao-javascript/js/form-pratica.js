c47b2193b2975ff7a814df66a6960cf77210c862
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2018-12-21 00:22:19 -0200

    Finalizar mod 6

--- introducao-javascript/js/form-pratica.js
+++ introducao-javascript/js/form-pratica.js
@@ -15,14 +15,13 @@ botaoAdicionar.addEventListener('click', function (event) {
 
     //Validação paciente
     var erros = validaPaciente(paciente);
-
+    console.log(erros);
     /**
      * No momento de chamarmos a função validaPaciente(paciente), extrairemos o retorno da função para a variável erro.
      * Se o tamanho da String for maior que 0, significa que ocorreu algum erro.
      */
     if (erros.length > 0) {
-        var mensagemErro = document.querySelector('.mensagem-erro');
-        mensagemErro.textContent = erros;
+        exibirMensagemErro(erros);
         return;
     }
 
@@ -30,7 +29,19 @@ botaoAdicionar.addEventListener('click', function (event) {
     var tabelaPaciente = document.querySelector('#tabela-pacientes');
     tabelaPaciente.appendChild(pacienteTr);
 
+    form.reset
+
 });
+
+
+function exibirMensagemErro(erros) {
+    var ul = document.querySelector('.mensagem-erro');
+    erros.forEach(function (erro) {
+        var li = document.createElement('li');
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obterFormPaciente(form) {
     var paciente = {
@@ -70,12 +81,29 @@ function montarTd(dado, classe) {
 function validaPaciente(paciente) {
 
     var erros = [];
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
 
     if (!validaAltura(paciente.altura)) {
         erros.push('Altura é inválida!');
+
     }
     if (!validaPeso(paciente.peso)) {
         erros.push('Peso é inválido!');
     }
+
+    if (paciente.gordura.length == 0) {
+        erros.push('A gordura não pode ficar em Branco!');
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push('A altura não pode ficar em Branco!');
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push('O peso não pode ficar em Branco!');
+    }
+
     return erros;
 }
