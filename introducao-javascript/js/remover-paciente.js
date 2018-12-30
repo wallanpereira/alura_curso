def49a7365e637024dfd6854f14651fada4273f6
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2018-12-30 12:36:37 -0200

    Verificar remover-paciente.js da aula 7

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,11 +1,26 @@
 var pacientes = document.querySelectorAll('.paciente');
 
-console.log(paciente);
+// var tabela = document.querySelector('#tabela-pacientes');
+var tabela = document.querySelector('table');
 
-pacientes.forEach(function (paciente) {
-    paciente.addEventListener('dblclick', function () {
-        console.log('Fui clicado duas vezes !!!');
-        this.remove();
-        console.log('Você removeu um paciente!')
-    })
+tabela.addEventListener('click', function (event) {
+    var alvoEvento = event.target;
+    var paiAlvo = paiAlvo.alvoEvento;
+    paiAlvo.remove();
+    // console.log('Fui clicado!!!');
+    // console.log(event.target);
+
+    // Retorna o elemento pai, ou seja, o que estiver na variavel tabela.
+    // console.log(this);
+
+    // Remove a célula clicada, movendo de sua direita para esquerda.
+    // event.target.remove();
 })
+
+// pacientes.forEach(function (paciente) {
+//     paciente.addEventListener('dblclick', function () {
+//         console.log('Fui clicado duas vezes !!!');
+//         this.remove();
+//         console.log('Você removeu um paciente!')
+//     })
+// })
