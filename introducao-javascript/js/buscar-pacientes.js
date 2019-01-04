89e560e47940485fadb8b423bac6c7b151490b5c
wallanpsantos <wallanpereira@hotmail.com>
2019-01-04 14:24:59 -0200

    Finalizando Aula 9 - Ajax

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -11,30 +11,36 @@ btnAdicionar.addEventListener('click', function (event) {
      */
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
-
     xhr.addEventListener('load', function () {
-        // Console abaixo retorna os dados de texto da API
-        // console.log(xhr.responseText);
-        var resposta = xhr.responseText;
-        // Console abaixo retorna o tipo da variavel
-        console.log(typeof resposta);
+        console.log(xhr.status)
+        if (xhr.status == 200) {
+            // Console abaixo retorna os dados de texto da API
+            // console.log(xhr.responseText);
+            var resposta = xhr.responseText;
+            // Console abaixo retorna o tipo da variavel
+            console.log(typeof resposta);
 
-        /** 
-         * JSON.parse() converte o dados da requisção em um objeto javascript. 
-         * Para conseguirmos transformar a resposta, que é um texto(uma string), em um array de pacientes, usaremos um "transformador", mais precisamente um parseador de JSON para objetos do JavaScript.Para realizarmos esta tarefa usaremos o método parse().Assim, receberemos o texto em JSON, que depois será parseado.Em seguida, será retornado um objeto JavaScript.Como nossa resposta se parece com um objeto, o método entenderá isso e nos retornará um array do objetos: 
-         * 
-         */
-        var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
-        // Console abaixo retorna tipo da variavel pacientes ( object )
-        console.log(typeof pacientes);
+            /** 
+             * JSON.parse() converte o dados da requisção em um objeto javascript. Usando parse() passaremos para texto. 
+             */
+            var pacientes = JSON.parse(resposta);
+            console.log(pacientes);
+            // Console abaixo retorna tipo da variavel pacientes ( object )
+            console.log(typeof pacientes);
 
-        /**
-         * Isto é, a função já montava um paciente, verificava os erros, e o adicionava na tabela.Nós poderemos chamar a função adicionaPacienteNaTabela() - com todas as ações mencionadas - , ou podemos aproveitá - la para adicionarmos os pacientes recebidos.No arquivo buscar - pacientes.js, adicionaremos adicionaPacienteNaTabela(), que por enquanto só inclui um paciente.No entanto, teremos um array com vários deles.Logo, iremos iterar pelo array usando o forEach(), e adicionaremos cada paciente contido nele.
-         */
-        pacientes.forEach(function (paciente) {
-            adicionarPacientesTabela(paciente);
-        })
+            /**
+             * Isto é, a função já montava um paciente, verificava os erros, e o adicionava na tabela.Nós poderemos chamar a função adicionaPacienteNaTabela() - com todas as ações mencionadas - , ou podemos aproveitá - la para adicionarmos os pacientes recebidos.No arquivo buscar - pacientes.js, adicionaremos adicionaPacienteNaTabela(), que por enquanto só inclui um paciente.No entanto, teremos um array com vários deles.Logo, iremos iterar pelo array usando o forEach(), e adicionaremos cada paciente contido nele.
+             */
+            pacientes.forEach(function (paciente) {
+                adicionarPacientesTabela(paciente);
+            });
+        } else {
+            // Caso ter algum erro no servidor mostra a mensagem
+            console.log(xhr.status);
+            // Selecionar class erro-ajax e invisivel da mensagem de erro e adicionar a uma variavel para tratar a mensagem.
+            var erroAjax = document.querySelector('.erro-ajax.invisivel');
+            erroAjax.classList.remove('invisivel');
+        }
     });
 
     xhr.send();
